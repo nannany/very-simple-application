49aa2d113acef1fb0aa6615ef5086647b2477048
nannany <ymym1990ymym@gmail.com>
2019-02-01 20:14:04 +0900

    first

---
+++ simple/src/main/java/nannany/App.java
@@ -0,0 +1,13 @@
+package nannany;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+        while (true) {
+            System.out.println("Hello World!");
+            Thread.sleep(1000L);
+        }
+    }
+}
